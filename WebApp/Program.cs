0fe463d6d14a9750f5c90f03725d231666e22a47
ManFis <vovka79025315990@gmail.com>
2023-10-16 19:40:40 +1000

    Create views for entities

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -31,6 +31,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Index}/{action=Index}/{id?}");
 
 app.Run();
